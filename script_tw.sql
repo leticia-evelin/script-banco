ce27c410f650530e5d71710074e5ef254b20130f
Leticia Evelin <ljkeleticia@gmail.com>
2023-05-31 21:53:01 -0300

    dados

--- script_tw.sql
+++ script_tw.sql
@@ -1,7 +1,7 @@
 create database db_tomorrows_water_2023;
  
 use db_tomorrows_water_2023;
- 
+show tables; 
 ##TABELA ONG 
 create table tbl_ong(
 			 id int not null auto_increment primary key,
@@ -25,7 +25,7 @@ insert into tbl_ong(
              'Saneamento e água potável',
              '1987-07-01'
              );
-
+select * from tbl_ong;
  
 ##TABELA PATROCINADOR
 create table tbl_patrocinador(
@@ -69,7 +69,7 @@ create table tbl_recado(
 			 sigla varchar(5) not null
 );
 
-insert into tbl_genero(nome, sigla) values ('Feminino', 'FEM'), ('Masculino', 'MASC');
+insert into tbl_genero(nome, sigla) values ('Feminino', 'F'), ('Masculino', 'M'), ('O', 'Outros');
                         
 
 ##TABELA PRODUTOS
@@ -95,6 +95,100 @@ create table tbl_produtos(
                         
 			unique index(id)
 ); 
+select * from tbl_produtos;
+
+insert into tbl_produtos(
+						nome, 
+                        descricao, 
+                        cor, 
+                        preco, 
+                        imagem, 
+                        altura, 
+                        largura, 
+                        comprimento,
+                        tamanho_sigla,
+                        peso,
+                        categoria,
+                        id_ong) values (
+                        'Camiseta TW', 
+                        'Camiseta de algodão Tomorrows Water',
+                        'Branco',
+                        '112.50',
+                        'https://i.im.ge/2023/05/24/hqDbi4.camiseta-frente.png',
+                        null,
+                        null,
+                        null,
+                        'M',
+                        '200g',
+                        'Roupa',
+                        1
+                        );
+
+insert into tbl_produtos(
+			nome, 
+            descricao, 
+            cor, preco, 
+            imagem, altura, 
+            largura, comprimento, 
+            tamanho_sigla, peso, 
+            categoria, id_ong
+            ) values (
+               'Ecobag sustentável',
+               'Ecobag da ONG. Nossa ideia é que nossos clientes deixem de usar sacolas plásticas que poluem nosso planeta e troque por sacolas reutilizáveis em algodão cru. Melhorando assim a condição do meio ambiente e a sustentabilidade.',
+               'Bege', '47',
+               'https://i.im.ge/2023/05/24/hqDccC.ecobag-tw-azul.png','40cm', 
+               '25mm','35cm',
+               null, '0.15g', 
+               'Produtos sustentáveis', 1
+				),
+                ('Garrafa Térmica Bambu TW',
+                'Garrafa Térmica com revestimento em bambu e aço inox, com parede dupla e infusor para chá. além da abundância e da beleza, o bambu ainda apresenta propriedades antibacterianas, é muito resistente, é leve, é versátil, é renovável e tem uma longa vida. Ecologicamente, o bambu cumpre um papel importantíssimo na proteção de nascentes e na preservação do solo firme.',
+                'Marrom', '99',
+                'https://i.im.ge/2023/05/24/hqDWCP.garrafa-logo.png', '20.4cm',
+                '6,9cm', '20cm',
+                null, '333g',
+                'Produtos para bebidas', 1
+                ),
+                ('Ecobag sustentável',
+				'Ecobag da ONG. Nossa ideia é que nossos clientes deixem de usar sacolas plásticas que poluem nosso planeta e troque por sacolas reutilizáveis em algodão cru. Melhorando assim a condição do meio ambiente e a sustentabilidade.',
+                'Bege', '47',
+                'https://i.im.ge/2023/05/24/hqDZv1.ecobag-tw-preto.png', '40cm',
+                '25mm', '35cm',
+                null, '0.15g',
+                'Produtos sustentáveis', 1
+                ),
+                ('Garrafa Térmica Bambu',
+                'Garrafa Térmica com revestimento em bambu e aço inox, com parede dupla e infusor para chá. além da abundância e da beleza, o bambu ainda apresenta propriedades antibacterianas, é muito resistente, é leve, é versátil, é renovável e tem uma longa vida. Ecologicamente, o bambu cumpre um papel importantíssimo na proteção de nascentes e na preservação do solo firme.',
+                'Marrom', '99',
+                'https://i.im.ge/2023/05/24/hqDkVr.garrafa-tw.png', '20.4cm',
+                '6.9cm', '20cm',
+                null, '333g',
+                'Produtos para as bebidas', 1
+                ),
+                ('Botton TW',
+                'Nossos bottons são feitos com o melhor material, desde o papel até o laminação do botton, tudo feito com muito carinho e cuidado para você! Para colocar na sua roupa, mochila, boné, etc...',
+                'Branco', '5',
+                'https://i.im.ge/2023/05/24/hqDf6m.botton-logo.png', '3.80cm',
+                '0.10cm', '3.80cm',
+                null, '50g',
+                'Acessórios', 1
+                ), 
+                ('Ecobag sustentável',
+                'Ecobag com logo da ONG. Nossa ideia é que nossos clientes deixem de usar sacolas plásticas que poluem nosso planeta e troque por sacolas reutilizáveis em algodão cru. Melhorando assim a condição do meio ambiente e a sustentabilidade.',
+                'Bege', '47',
+                'https://i.im.ge/2023/05/24/hqDV2f.ecobag-logo.png', '40cm',
+                '25mm', '35cm'
+                'null', '0.15g',
+                'Produtos sustentáveis', 1
+                ),
+                ('Botton Tomorrow',
+                'Nossos bottons são feitos com o melhor material, desde o papel até o laminação do botton, tudo feito com muito carinho e cuidado para você! Para colocar na sua roupa, mochila, boné, etc...',
+                'Branco', '5',
+                'https://i.im.ge/2023/05/24/hdWe8m.botton-tw.png', '3.80cm',
+                '0.10cm', '3.80cm',
+                null, '50g',
+                'Acessórios', 1
+                );
 
 
 ##TABELA DOADOR
@@ -178,6 +272,20 @@ create table tbl_projetos(
                         
 			unique index(id)
 );
+select * from tbl_projetos;
+insert into tbl_projetos(
+			nome, descricao, imagem, id_ong
+            ) values (
+            'Saúde',
+            'Nosso principal projeto envolvido na área da saúde é levar a água potável para as pessoas com maior careza no acesso a este bem tão precioso. Nosso foco está no munícipio do macapá onde a situação é alarmante, a distribuição das garrafinhas de água será feita por voluntários da nossa ong.',
+            'imagem aqui', 1), (
+            'Higiene',
+            'Nesta área buscamos zelar pela higiene digna de todas as pessoas independentemente das condições em que vivem e onde vivem, para isso vamos levar postos de higienização pessoal que serão barcos com banheiros, para que a higienização seja mais acessivel e não seja um problema se tornando parte da rotina.',
+            'imagem aqui', 1),
+            ( 'Bem estar',
+            'Buscamos trazer um local para viver digno a todos os cidadões, sem a presença de esgotos a céu aberto e outros problemas devidos a falta de saneamento básico, para sanarmos com isso vamos realizar obras de melhoria em lugares precários e esquecidos na região do macapá.',
+            'imagem aqui', 1);
+
 
 ##TABELA CIDADE
 create table tbl_cidade(
